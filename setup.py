97e0f5d464a71e4c6b0ba80703c15e4477e2df35
Erica Fu <ericafu@renci.org>
2020-05-04 15:20:02 -0400

    update package files

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 import setuptools
 
+VERSION = "0.0.1.4"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="fabricauthenticator",  # Replace with your own username
-    version="0.0.1",
+    name="fabricauthenticator",
+    version=VERSION,
+    author="Erica Fu",
+    author_email="ericafu@renci.org",
     description="Fabric Authenticator for Jupyterhub",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fabric-testbed/fabricauthenticator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
